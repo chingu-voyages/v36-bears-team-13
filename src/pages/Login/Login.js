7c25fba51ce3ca2dac7c0329b52465ada0368224
Davor Pernjek <pernjek.davor12@gmail.com>
2022-01-16 12:00:19 +0100

    fix login not in center

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className=" flex justify-center items-center w-full">
+    <div className="flex justify-center items-center w-full h-full">
       <form>
         <div className="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
           <div className="space-y-4">
             <h1 className="text-center text-2xl font-semibold text-gray-600">Login</h1>
             <div>
-              <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">
+              <label htmlFor="username" className="block mb-1 text-gray-600 font-semibold">
                 Username
               </label>
               <input
@@ -17,11 +17,11 @@ const Login = () => {
               />
             </div>
             <div>
-              <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">
+              <label htmlFor="password" className="block mb-1 text-gray-600 font-semibold">
                 Password
               </label>
               <input
-                type="text"
+                type="password"
                 className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
               />
             </div>
