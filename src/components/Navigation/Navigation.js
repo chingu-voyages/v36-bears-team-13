59af4fc15aaabae320dc86c9bc449acfb7a200ab
Davor Pernjek <pernjek.davor12@gmail.com>
2022-01-16 12:00:04 +0100

    navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,27 +3,29 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>
-      <nav className="font-sans flex flex-none flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
-        <div className="mb-2 sm:mb-0 flex bg-slate-500">
-          <Link to="/" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
-            NannyNow
-          </Link>
-        </div>
-        <div className="flex justify-between">
-          <Link
-            to="/login"
-            className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
-            Login
-          </Link>
-          <Link
-            to="/register"
-            className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
-            Register
-          </Link>
-        </div>
-      </nav>
-    </div>
+    <nav className="font-sans flex flex-none flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
+      <div className="mb-2 sm:mb-0 flex-1 ">
+        <Link to="/" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
+          NannyNow
+        </Link>
+      </div>
+      <div className="flex flex-1 space-x-4 justify-end">
+        <Link to="/" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
+          Home
+        </Link>
+        <Link to="/create" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
+          Create advert
+        </Link>
+        <Link to="/login" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
+          Login
+        </Link>
+        <Link
+          to="/register"
+          className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
+          Register
+        </Link>
+      </div>
+    </nav>
   );
 };
 
