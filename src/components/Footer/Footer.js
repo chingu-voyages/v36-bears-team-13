502f5a43f9fcae4f85b0f0e3197900818a8ca113
Davor Pernjek <pernjek.davor12@gmail.com>
2022-01-16 11:59:51 +0100

    change footer color

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-gray-900 flex-none">
+    <div className="bg-indigo-900 flex-none">
       <footer className="flex flex-wrap items-center justify-between p-3 m-auto">
         <div className="container mx-auto flex flex-col flex-wrap items-center justify-between">
           <div className="flex mx-auto text-white text-center">Copyright NannyNow © 2022</div>
