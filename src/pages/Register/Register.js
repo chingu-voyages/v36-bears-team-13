1b08b4c1485ea1d5bd7bcc93b1083af8c39f3f39
Davor Pernjek <pernjek.davor12@gmail.com>
2022-01-16 12:00:29 +0100

    fix register not in center

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -2,14 +2,50 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div className="bg-gradient-to-br from-blue-600 to-indigo-600 flex justify-center items-center w-full">
+    <div className="flex justify-center items-center w-full h-full">
       <form>
         <div className="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
           <div className="space-y-4">
             <h1 className="text-center text-2xl font-semibold text-gray-600">Register</h1>
             <div>
-              <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">
+              <label htmlFor="username" className="block mb-1 text-gray-600 font-semibold">
                 Username
+              </label>
+              <input
+                type="text"
+                className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
+              />
+            </div>
+            <div>
+              <label htmlFor="firstname" className="block mb-1 text-gray-600 font-semibold">
+                First name
+              </label>
+              <input
+                type="text"
+                className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
+              />
+            </div>
+            <div>
+              <label htmlFor="lastname" className="block mb-1 text-gray-600 font-semibold">
+                Last name
+              </label>
+              <input
+                type="text"
+                className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
+              />
+            </div>
+            <div>
+              <label htmlFor="gender" className="block mb-1 text-gray-600 font-semibold">
+                Gender
+              </label>
+              <input
+                type="text"
+                className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
+              />
+            </div>
+            <div>
+              <label htmlFor="role" className="block mb-1 text-gray-600 font-semibold">
+                Role
               </label>
               <input
                 type="text"
@@ -21,16 +57,17 @@ const Register = () => {
                 Email
               </label>
               <input
-                type="text"
+                type="email"
                 className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
               />
             </div>
+
             <div>
-              <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">
+              <label htmlFor="password" className="block mb-1 text-gray-600 font-semibold">
                 Password
               </label>
               <input
-                type="text"
+                type="password"
                 className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
               />
             </div>
