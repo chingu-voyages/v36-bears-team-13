d7ef1c0fca0685925a8d34144bf8e83ade472786
Davor Pernjek <pernjek.davor12@gmail.com>
2022-01-16 12:01:00 +0100

    create home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AdCard from "../../components/AdCard/AdCard";
+
+const Home = () => {
+  return (
+    <div className="flex-1 bg-gradient-to-br from-indigo-100 to-indigo-200 justify-center items-center">
+      <AdCard />
+      <AdCard />
+      <AdCard />
+      <AdCard />
+      <AdCard />
+    </div>
+  );
+};
+
+export default Home;
