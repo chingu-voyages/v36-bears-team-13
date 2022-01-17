04b66043b5dfe75349a756da913709586bc64906
Davor Pernjek <pernjek.davor12@gmail.com>
2022-01-17 17:58:17 +0100

    wills adverts

---
+++ src/pages/Home/Advert.js
@@ -0,0 +1,63 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Advert = ({
+  name,
+  description,
+  availability,
+  area,
+  childAge,
+  price,
+  gender,
+  experience,
+  email,
+  phone
+}) => {
+  console.log("name", name);
+  return (
+    <li>
+      <section className="advert">
+        <div className="main">
+          <hgroup>
+            <h1 className="name">{name}</h1>
+          </hgroup>
+          <p className="description">{description}</p>
+          <div className="main-details">
+            <p className="availability">Availability: {availability}</p>
+            <div className="area-age">
+              <p>Area: {area}</p>
+              <p>Child age: {childAge}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="details">
+          <div className="differentiators">
+            <p>Price: {price}</p>
+            <p>Gender: {gender}</p>
+            <p>Experience: {experience}</p>
+          </div>
+          <div className="contact-details">
+            <p>Email: {email}</p>
+            <p>Phone: {phone}</p>
+          </div>
+        </div>
+      </section>
+    </li>
+  );
+};
+
+export default Advert;
+
+Advert.propTypes = {
+  description: PropTypes.string,
+  name: PropTypes.string,
+  area: PropTypes.string,
+  availability: PropTypes.string,
+  childAge: PropTypes.string,
+  price: PropTypes.string,
+  gender: PropTypes.string,
+  experience: PropTypes.string,
+  email: PropTypes.string,
+  phone: PropTypes.string
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,27 @@
 import React from "react";
-import AdCard from "../../components/AdCard/AdCard";
-
+import { ads } from "./dummy";
+import Advert from "./Advert";
 const Home = () => {
+  console.log("ad", ads);
   return (
     <div className="flex-1 bg-gradient-to-br from-indigo-100 to-indigo-200 justify-center items-center">
-      <AdCard />
-      <AdCard />
-      <AdCard />
-      <AdCard />
-      <AdCard />
+      <ul id="adverts">
+        {ads.map((ad) => (
+          <Advert
+            key={ad.id}
+            description={ad.description}
+            name={ad.name}
+            availability={ad.availability}
+            area={ad.area}
+            childAge={ad.childAge}
+            price={ad.price}
+            gender={ad.gender}
+            exeprience={ad.experience}
+            email={ad.email}
+            phone={ad.phone}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
