007075ef2c5c2891bdcbc214eb6c050b53310c5e
Davor Pernjek <pernjek.davor12@gmail.com>
2022-01-18 18:04:22 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import "./style/main.css";
+import Navigation from "./components/Navigation/Navigation.js";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Home from "./pages/Home/Home";
+import Auth from "./components/Layouts/Auth/Auth";
+import CreateAd from "./pages/CreateAd/CreateAd";
 function App() {
   return (
-    <div>
-      <p>Nanny Now</p>
+    <div className="flex flex-col h-screen">
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route element={<Auth />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route exact path="/register" element={<Register />}></Route>
+          </Route>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/create" element={<CreateAd />}></Route>
+        </Routes>
+      </Router>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-indigo-900 flex-none">
+      <footer className="flex flex-wrap items-center justify-between p-3 m-auto">
+        <div className="container mx-auto flex flex-col flex-wrap items-center justify-between">
+          <div className="flex mx-auto text-white text-center">Copyright NannyNow © 2022</div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
