3933ad345c887a206db93851d68d8077378d231d
Will <wwhite9862@gmail.com>
2022-01-31 14:07:44 +0000

    separate parts of advert

--- src/components/AdCard/AdCard.js
+++ src/components/AdCard/AdCard.js
@@ -2,37 +2,27 @@ import React from "react";
 
 const AdCard = (ad) => {
   return (
-    <aside className="advert">
+    <aside>
       <article>
-        <div className="main">
-          <hgroup>
-            <h3 className="name">{ad.name}</h3>
-          </hgroup>
-          <p className="description">{ad.description}</p>
-          <div className="main-details">
-            <p className="availability">Availability: {ad.availability}</p>
-            <div className="area-age">
-              <p>Area: {ad.area}</p>
-              <p>Child age: {ad.childAge}</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="details">
-          <div className="differentiators">
-            <p>Price: {ad.price}</p>
-            <p>Experience: {ad.experience}</p>
-            <p>Gender: {ad.gender}</p>
-          </div>
-          <div className="contact-details">
-            <small>
-              <address>{ad.email}</address>
-            </small>
-            <small>
-              <address>{ad.phone}</address>
-            </small>
-          </div>
-        </div>
+        <h3>{ad.name}</h3>
+        <p>{ad.description}</p>
+        {/* Separate details from name and description */}
+        <hr />
+        <p>Availability: {ad.availability}</p>
+        <p>Area: {ad.area}</p>
+        <p>Child age: {ad.childAge}</p>
+        <hr />
+        <p>Price: {ad.price}</p>
+        <p>Experience: {ad.experience}</p>
+        <p>Gender: {ad.gender}</p>
+        <hr />
+        {/* Separate email and phone from other details */}
+        <small>
+          <address>{ad.email}</address>
+        </small>
+        <small>
+          <address>{ad.phone}</address>
+        </small>
       </article>
     </aside>
   );
